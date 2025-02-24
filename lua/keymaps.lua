f00aa5cb5170bfe74e157ebb1a38cdd499d605af
Menaka Jayawardena <menaka12350@gmail.com>
2025-02-24 23:54:46 +0530

    Add harpoon

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -31,6 +31,12 @@ vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right win
 vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
 vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
 
+vim.keymap.set('n', '<leader>to', '<cmd>tabnew<CR>', { desc = 'Open new tab' }) -- open new tab
+vim.keymap.set('n', '<leader>tx', '<cmd>tabclose<CR>', { desc = 'Close current tab' }) -- close current tab
+vim.keymap.set('n', '<leader>tn', '<cmd>tabn<CR>', { desc = 'Go to next tab' }) --  go to next tab
+vim.keymap.set('n', '<leader>tp', '<cmd>tabp<CR>', { desc = 'Go to previous tab' }) --  go to previous tab
+vim.keymap.set('n', '<leader>tf', '<cmd>tabnew %<CR>', { desc = 'Open current buffer in new tab' }) --  move current buffer to new tab
+
 vim.keymap.set('n', '\\', ':NvimTreeFindFileToggle<CR>', { desc = 'Open NvimTreeFindFileToggle' })
 
 -- [[ Basic Autocommands ]]
--- lua/lazy-plugins.lua
+++ lua/lazy-plugins.lua
@@ -60,6 +60,7 @@ require('lazy').setup({
   require 'menaka.plugins.autopairs',
   require 'menaka.plugins.neo-tree',
   require 'menaka.plugins.avante',
+  require 'menaka.plugins.harpoon',
 
   -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
   --    This is the easiest way to modularize your config.
